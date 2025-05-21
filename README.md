99845cd402064f189504ae3dd15cfc3a9411eddb
KARL <karl122701@gmail.com>
2025-05-21 18:21:15 -0400

    Refactor Javascript for modularity.

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
 # Simple Todo App
 
-Give a brief description of the project here. Feel free to give it a different name.
+This to-do application allows you to create tasks with due dates. This application allows you to check tasks that have been completed and also allows the deletion of tasks.
 
 ## Functionality
 
-Give a more detailed explanation of the project and its functionality.
+This to-do application features validation when entering a new task, requiring at least 2 letters be entered before accepting a result. This application allows the deletion of created tasks, and also allows the user to check off tasks to indicate their completion.
 
 ## Technology
 
-Give a description of the technologies and techniques used. Pictures, GIFs, or screenshots that detail the project features are recommended.
+This application was created using HTML, CSS, and Javscript. The technologies used for Javascript include encapsulation, which is the usage of private/public classes for easy access, ES6+ features such as arrow functions, default & named exports, and template literals, querying and manipulating DOM elements, and splitting the code into modules to accomplish a modular Javascript structure.
+
+Default Desktop View:
+![alt text](./images/image.png)
+
+Default Mobile View:
+![alt text](./images/image-1.png)
+
+Desktop View Add Task Modal:
+![alt text](./images/image-3.png)
+
+Mobile View Add Task Modal:
+![alt text](./images/image-2.png)
 
 ## Deployment
 
---
+++ components/Todo.js
@@ -0,0 +1,62 @@
+class Todo {
+  constructor(data, selector) {
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+
+  _date() {
+    this._dueDate = new Date(this._data.date);
+    if (!isNaN(this._dueDate)) {
+      this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
+        "en-US",
+        {
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        }
+      )}`;
+    }
+  }
+
+  _deleteListener() {
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+  }
+
+  _setEventListeners() {
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = this._todoCheckboxEl.checked;
+      console.log(this._data.completed);
+      console.log("Checkbox changed");
+    });
+  }
+
+  _generateCheckboxEL() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+  }
+
+  getView() {
+    this._todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+    const todoNameEl = this._todoElement.querySelector(".todo__name");
+
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+
+    todoNameEl.textContent = this._data.name;
+
+    this._date();
+    this._generateCheckboxEL();
+    this._setEventListeners();
+    this._deleteListener();
+    return this._todoElement;
+  }
+}
+
+export default Todo;
